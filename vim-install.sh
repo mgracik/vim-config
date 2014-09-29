f01ce168c0a9ab5ac7a2c5a1edd6fc206739f1b9
Martin <martin@gracik.me>
2014-09-29 20:26:12 +0200

    Update broken URLs

--- vim-install.sh
+++ vim-install.sh
@@ -6,7 +6,7 @@ VIMDIR="$HOME/.vim"
 
 sudo apt-get install -y vim-gnome vim-doc ttf-dejavu exuberant-ctags curl
 mkdir -p $VIMDIR/autoload
-curl -Sso $VIMDIR/autoload/pathogen.vim https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
+curl -Sso $VIMDIR/autoload/pathogen.vim https://raw.githubusercontent.com/tpope/vim-pathogen/master/autoload/pathogen.vim
 git clone https://github.com/mgracik/vim-bundle.git $VIMDIR/bundle
 { cd $VIMDIR/bundle && git submodule update --init --recursive; }
-curl -Sso $HOME/.vimrc https://raw.github.com/mgracik/vim-config/master/vimrc
+curl -Sso $HOME/.vimrc https://raw.githubusercontent.com/mgracik/vim-config/master/vimrc
