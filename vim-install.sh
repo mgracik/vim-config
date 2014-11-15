c7cd1294a8b1315744c79e9daae9700ca86d3a19
Martin <martin@gracik.me>
2014-11-15 22:42:06 +0100

    Initialize plugins in a subshell

--- vim-install.sh
+++ vim-install.sh
@@ -8,5 +8,5 @@ sudo apt-get install -y vim-gnome vim-doc ttf-dejavu exuberant-ctags curl
 mkdir -p $VIMDIR/autoload
 curl -Sso $VIMDIR/autoload/pathogen.vim https://raw.githubusercontent.com/tpope/vim-pathogen/master/autoload/pathogen.vim
 git clone https://github.com/mgracik/vim-bundle.git $VIMDIR/bundle
-{ cd $VIMDIR/bundle && git submodule update --init --recursive; }
+( cd $VIMDIR/bundle && git submodule update --init --recursive; )
 curl -Sso $HOME/.vimrc https://raw.githubusercontent.com/mgracik/vim-config/master/vimrc
